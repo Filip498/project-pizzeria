3293e10f05dd71921efcbc73f8771ed626ca40c4
Filip Cheliński <filipchel20@gmail.com>
2022-09-17 16:20:45 -0200

    Add a subpage

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,59 @@
-import { select, settings} from './settings.js';
+import { classNames, select, settings} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function() {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages) {
+      if(page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+
+  },
+
+  activatePage: function(pageID) {
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageID);
+    }
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' +  pageID
+      );
+    }
+
+  },
   initMenu: function(){
     const thisApp = this;
 
@@ -28,6 +79,7 @@ const app = {
   initData: function(){
     const thisApp = this;
 
+    thisApp.data = {};
     const url = settings.db.url + '/' + settings.db.products;
     fetch(url)
       .then(function(rawResponse){
@@ -38,11 +90,9 @@ const app = {
         /* save parsedResponse as thisApp.data.products */
         thisApp.data.products = parsedResponse;
         /*execute initMenu method */
-        return thisApp.initMenu();
+        thisApp.initMenu();
       });
     //console.log('thisApp.data', JSON.stringify(thisApp.data));
-
-    thisApp.data = {};
   },
 
   init: function(){
@@ -52,7 +102,8 @@ const app = {
     //console.log('classNames:', classNames);
     //console.log('settings:', settings);
     //console.log('templates:', templates);
-
+    
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
